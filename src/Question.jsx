95d4aad3e8399d6fb690e3b32a9fcfe2c618befe
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-05 21:45:25 +0100

    features: created a useState varible

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <article className="question">
       <header>
